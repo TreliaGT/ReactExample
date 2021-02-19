d43803103c9cb70b6ab2a45927f6c98073a7db04
Georgia <coder@secretworks.com.au>
2021-02-19 16:17:59 +0800

    ageg

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,54 @@
-import React, {Component} from 'react';
+import React, {Component , useEffect} from 'react';
+
 import '../App.css';
-import stars from '../JS/stars.js'
 
+function CreateStars(type, qty ) {
+    for(let i = 0; i < qty; i++) {
+        let star = document.createElement('div');
+        star.classList.add('star', 'type-' + type);
+        star.style.left = RandomNumber(1, 99) + '%';
+        star.style.bottom = RandomNumber(1, 99) + '%';
+        star.style.animationDuration = RandomNumber(50, 200) + 's';
+        document.getElementById('stars-wrapper').appendChild(star);
+    }
+}
 
+function RandomNumber(min, max) {
+    return Math.floor(Math.random() * max) + min;
+}
 
-
+function Stars(){
+        CreateStars(1, 100 );
+        CreateStars(2, 85 );
+        CreateStars(3, 70 );
+}
 
 
 class Home extends Component {
     constructor(props) {
         super(props);
+        this.myInput = React.createRef();
+    
     }
 
+    componentDidMount() {  
+        Stars(); 
+       }
+    
     render() {
         return (
                 <div className="container">
                     <div className="Home">
                         <div className="Banner">
-                            <div id="stars-wrapper" >aaaa</div>
-                            <script src={stars}></script>
+                            <div Ref={this.myInput} id="stars-wrapper" className="stars-wrapper"></div>
                         </div>
                     </div>
                 </div>
         );
+        
     }
 }
+
 
 
 export default Home;
