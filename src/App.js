d83a97dc0e0ec5b39550679fa4e8bb7e1cf13215
Georgia <coder@secretworks.com.au>
2021-02-19 12:55:39 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import HomePage from './pages/Home';
+import NotFound from './pages/Error404';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+  } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+    constructor() {
+        super();
+    }
+
+
+    render() {
+        return (
+          <Router>
+          <div className="App">
+            <header>
+              <div className="Logo">
+                  <img src="../logo.svg"/>
+              </div>
+              <div className="Nav">
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/">About</Link>
+                  </li>
+                  <li>
+                    <Link to="/">Projects</Link>
+                  </li>
+                  <li>
+                    <Link to="/">Contact</Link>
+                  </li>
+                </ul>
+              </div>
+            </header>
+            <Switch>
+              <Route path="/">
+                <HomePage/>
+              </Route>
+              <Route path="*">
+                <NotFound/>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+        );
+      }
+  }
+
+
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import '../App.css';
+
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return (
+                <div className="container">
+                    <div className="Home">
+                        <div className="Banner">
+
+
+
+                        </div>
+                    </div>
+                </div>
+        );
+    }
+}
+
+export default Home;
